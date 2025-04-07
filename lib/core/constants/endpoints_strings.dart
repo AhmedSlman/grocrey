b54842eb1a0e9d11175f7e9b353a7e1ebc28dc1d
AhmedSlman <ahmedslman151@gmail.com>
2025-04-07 16:42:00 +0200

    remove unused API endpoint constants and clean up router configuration

--- lib/core/constants/endpoints_strings.dart
+++ lib/core/constants/endpoints_strings.dart
@@ -8,37 +8,6 @@ abstract class EndpointsStrings {
   static const String verifyEmail = 'api/verify-email';
   static const String forgetPassword = 'api/reset-password-send-code';
   static const String checkCode = 'api/reset-password-check-code';
-  static const String resetPassword = 'api/reset-password';
 
-  static const String resendCode = 'api/resend-code';
-  static const String setLocation = 'api/set-location';
-  static const String banners = 'api/banners';
 
-  static const String allCategories = 'api/categories';
-  static const String categoryProducts = 'api/categories/';
-  static const String productDetails = 'api/product/';
-
-  static const String bestSeller = 'api/most-sold-products';
-  static const String dailyOrder = 'api/offers';
-  static const String addToFavorite = 'api/toggle-favourite/';
-
-  static const String getProfile = 'api/get-profile';
-  static const String changePassword = 'api/change-password';
-  static const String updateProfile = 'api/update-profile';
-  static const String getMyOrders = 'api/orders';
-  static const String getOrderDetails = 'api/orders';
-  static const String getCart = 'api/cart-summary';
-  static const String addToCart = 'api/add-to-cart';
-  static const String deleteFromCart = 'api/delete-cart';
-  static const String updateCart = 'api/update-cart';
-  static const String getQuestions = 'api/faqs';
-  static const String getPrizeProducts = 'api/prize-products';
-  static const String getExtraPointsOptions = 'api/settings';
-  static const String getUserPoints = 'api/points';
-  static const String rateApp = 'api/rate-app';
-
-  static const String getAddresses = 'api/addresses';
-  static const String storeAddress = 'api/store-address';
-  static const String deleteAddress = 'api/delete-address/';
-  static const String checkOut = 'api/store-order';
 }
